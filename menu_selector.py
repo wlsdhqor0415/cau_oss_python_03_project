d2faaf84f29c6dcc88cb386937eeef54a3576834
Baek Jino <wlsdhqor0415@gmail.com>
2023-06-16 16:09:02 +0900

    added .gitignore, modified 3 files and added comments to implement version#2

--- menu_selector.py
+++ menu_selector.py
@@ -1,4 +1,19 @@
+"""
+menu_selector 모듈은 메뉴와 관련된 함수를 제공하는 모듈입니다.
+start_process 함수로 메뉴 화면을 불러오고 사용자의 선택에 따라 각 함수를 호출합니다.
+"""
+import file_manager
+import parking_spot_manager
+
 def start_process(path):
+    str_list = file_manager.read_file(path)
+    spots = parking_spot_manager.str_list_to_class_list(str_list)
+    """
+    strat_process 함수는 file_manager 모듈에서의 read_file 함수와 
+    parking_spot_manager 모듈에서의 str_list_to_class_list 함수를 
+    호출해 parking_spot_manger 모듈의 parking_spot 클래스 객체의 리스트를 생성합니다.
+    사용자가 입력하는 select 값에 따라 함수를 불러옵니다.
+    """
     while True:
         print("---menu---")
         print("[1] print")
@@ -7,8 +22,11 @@ def start_process(path):
         print("[4] exit")
         select = int(input('type:'))
         if select == 1:
-            print("not implemented yet")
-            # fill this block
+            """
+            menu의 1번 [print] 기능은 parking_spot_manager 모듈의 print_spots 함수를 이용해
+            parking_spot 클래스 객체의 리스트 spots를 출력하는 것입니다.
+            """
+            parking_spot_manager.print_spots(spots)
         elif select == 2:
             print("---filter by---")
             print("[1] name")
@@ -52,7 +70,10 @@ def start_process(path):
                 # fill this block
             else: print("invalid input")
         elif select == 4:
-            print("not implemented yet")
-            # fill this block
+            """
+            menu의 4번 [exit] 기능은 "Exit"를 출력하고 프로그램을 종료하는 것입니다.
+            """
+            print("Exit")
+            break
         else:
             print("invalid input")
--- parking_spot_manager.py
+++ parking_spot_manager.py
@@ -1,11 +1,106 @@
+"""
+parking_spot_manager 모듈은 주차장의 정보와 관련된 함수 및 클래스를 제공하는 모듈입니다.
+parking_spot 클래스를 이용하여 주차장의 정보를 설정하고 참조하는 것을 수행합니다.
+str_list_to_class_list 함수를 통해 클래스 객체 리스트를 반환합니다.
+print_spots 함수를 통해 리스트의 원소 개수와 리스트에 저장된 모든 객체의 값을 출력합니다.
+"""
 class parking_spot:
-    # you have to implement 'constructor(생성자)' and 'get' method
+    """
+    parking_spot 클래스는 주차장의 이름, 시도, 시군구, 주차장의 유형, 
+    주차장의 경도, 주차장의 위도에 대해 저장하고 있는 클래스입니다
+    변수로는 외부에서 접근 불가능한 __item이 있으며,
+    해당 변수의 생성과 설정은 생성자를 통해서만 이루어집니다.
+    해당 변수를 접근하기 위한 메소드로는 get이 있습니다.
+    """
+    def __init__(self, name, city, district, ptype, longitude, latitude):
+        """
+        생성자 초기 주차장의 이름, 시도, 시군구, 주차장의 유형, 
+        주차장의 경도, 주차장의 위도 값을 받아 __item 딕셔너리에 저장합니다.
+        Args:
+            name (str): 초기 주차장의 이름 값입니다.
+            city (str): 초기 시도 값입니다.
+            district (str): 초기 시군구 값입니다.
+            ptype (str): 초기 주차장의 유형 값입니다.
+            longitude (float): 초기 주차장의 경도 값입니다.
+            latitude (float): 초기 주차장의 위도 값입니다.
+        """
+        self.__item = { 'name': name,
+                       'city': city,
+                       'district': district,
+                       'ptype': ptype,
+                       'longitude': longitude,
+                       'latitude': latitude}
+        
+    def get(self, keyword):
+        """
+        객체가 저장하고 있는 정보를 반환합니다.
+        Args:
+            keyword (str): 객체의 __item 딕셔너리의 key 값을 받습니다.
+                              객체에 저장된 정보 중에서 어떤 정보를 
+                              반환할 것인지를 결정합니다.
+        Returns:
+            str or float: 저장하고 있는 주차장의 이름, 시도, 
+                             시군구, 유형 값은 string으로
+                             주차장의 경도, 위도 값은 float로 반환합니다.
+        """
+        if keyword == 'name':
+            return self.__item['name']
+        elif keyword == 'city':
+            return self.__item['city']
+        elif keyword == 'district':
+            return self.__item['district']
+        elif keyword == 'ptype':
+            return self.__item['ptype']
+        elif keyword == 'longitude':
+            return self.__item['longitude']
+        elif keyword == 'latitude':
+            return self.__item['latitude']
+        else:
+            print("Invalid option")
+
     def __str__(self):
+        """
+        객체가 저장하고 있는 정보를 출력합니다.
+        Returns:
+            str: 저장하고 있는 주차장의 이름, 시도, 시군구, 유형, 
+                    경도, 위도 값을 하나의 문자열로 묶어서 반환합니다.  
+        """
         item = self.__item
         s  = f"[{item['name']}({item['ptype']})] "
         s += f"{item['city']} {item['district']}"
         s += f"(lat:{item['latitude']}, long:{item['longitude']})"
         return s
+
+def str_list_to_class_list(str_list):
+    """
+    str_list_to_class_list 함수는 문자열 리스트 str_list를 매개변수로 
+    받아 parking_spot 클래스 객체의 리스트로 변환하는 함수입니다.
+    Args:
+        str_list (list): 문자열 리스트입니다.
+    Returns:
+        list: parking_spot 클래스 객체의 리스트를 반환합니다.
+    """
+    mylist = []
+    for item in str_list:
+        data = item.split(',')
+        name = data[1]
+        city = data[2]
+        district = data[3]
+        ptype = data[4]
+        longitude = float(data[5])
+        latitude = float(data[6])
+        mylist.append(parking_spot(name, city, district, ptype, longitude, latitude))
+    return mylist
+
+def print_spots(mylist):
+    """
+    print_spots 함수는 리스트를 받아 출력합니다.
+    Args: 
+        mylist (list): 입력받는 parking_spot 클래스 객체의 리스트입니다.
+    """
+    print(f"---print elements({len(mylist)})---")
+    for i in range(len(mylist)):
+        print(mylist[i].__str__())
 
 
 # 각 단계별로 테스트 (테스트할때 주석해제 후 사용)
@@ -13,7 +108,7 @@ if __name__ == '__main__':
     print("Testing the module...")
     # version#2
     # import file_manager
-    # str_list = file_manager.from_file("./input/free_parking_spot_seoul.csv")
+    # str_list = file_manager.read_file("./input/free_parking_spot_seoul.csv")
     # spots = str_list_to_class_list(str_list)
     # print_spots(spots)
 
